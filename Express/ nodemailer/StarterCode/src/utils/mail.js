4c81921da542b87ed4acd2dc21004d97493ef98c
Abhishek Gupta <abhishek9661342993@gmail.com>
2024-08-16 08:19:08 +0530

    Update mail.js
    add commiet

--- Express/ nodemailer/StarterCode/src/utils/mail.js
+++ Express/ nodemailer/StarterCode/src/utils/mail.js
@@ -2,13 +2,23 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
+    host: "smtp.gmail.com", // SMTP server hostname 
     port: 465,
     secure: true, // Use `true` for port 465, `false` for all other ports
     auth: {
-      user: "abhishek9661342993@gmail.com",
-      pass: "pkmc nlkp jmcx mlma",
+      user: "your email id", //  Email address used for authentication
+      pass: "your password", //   Password for the email address 
+      
     },
   });
 
 export default transporter;
+
+//  Where to get password 
+//  1. Open Gmail
+//  2. Click on your profile picture
+//  3. Click on manage your google account
+//  4 . Search App password and click on it
+// 5. It will ask App name Enter any name Ex :- myApp
+//  6. It will create a randam password copy it 
+// 7. and past inside pass   pass: "your password", //   Password for the email address 
