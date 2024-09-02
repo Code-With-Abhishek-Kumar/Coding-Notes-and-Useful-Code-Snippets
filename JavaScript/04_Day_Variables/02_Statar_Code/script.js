9b317d12ce248e9f3636a3dcdb405ce9c23c81f4
Abhishek Kumar <abhishek9661342993@gmail.com>
2024-09-02 10:50:39 +0530

    ♻️  variable  javaScript code modified   , delete and add comment

--- JavaScript/04_Day_Variables/02_Statar_Code/script.js
+++ JavaScript/04_Day_Variables/02_Statar_Code/script.js
@@ -1,64 +1,36 @@
+// Variable Declaration and Assignment
 
+// Declare and assign a variable in one step
+let carName = "Volvo";
+console.log(`Declaring a JavaScript Variable: ${carName}`);
 
-let carName;  
-console.log("Declaring a JavaScript Variable " , carName = "Volvo")
- 
-console.log("To assign a value to the variable, use the equal sign:" ,carName = "Volvo")
-console.log("Your variable assign Volvo " + carName)
+// Avoid reassigning a variable immediately after declaration
+// Instead, use a new variable or reassign later in the code
+let newCarName = "Toyota";
+console.log(`Reassigning a variable: ${newCarName}`);
 
-// add two number
+/* -------------------------------------------------------------------------- */
+/* -----------------  🚫 Don't use var to declare variable  ---------------- */
+/* ------------- Because of issue in block scope and functional scope  --------- */
+/* -----------------It is not good practice to use var in js------------------ */
+/* -------------------------------------------------------------------------- */
+
+// Use let or const instead of var
+let num = 12;
+console.log(`Printing num variable: ${num}`);
+
+/* -------------------------------------------------------------------------- */
+/*     this will throw an Error because we can't change the value of const   */
+/* -------------------------------------------------------------------------- */
+const pi = 22 / 7;
+console.log(`The value of pi is: ${pi}`);
+
+// Type Checking
 
 let num1 = 12;
-console.log( "find type of variable using type of :-" , typeof(num1))
-let num2 = 15;
-let num3 = 30;
+console.log(`Find type of variable using typeof: ${typeof num1}`);
 
-
-
-
-
-
-// Scope of variables and functions
-
-
-console.log("1. Block Scope:")
-
-// This variable 'x' is accessible throughout the script
-let x = 10;  // Global Variable
-
-// This is a block of code
-{
-    // This variable 'y' is only accessible within this block
-    let y = 20; // local variable
-    console.log(y); // Output: 20
-}
-
-console.log(x); // Output: 10
-// console.log(y); // This would cause an error because 'y' is not accessible here
-
-console.log("2. Function Scope")
-
-// Global scope variable
-let globalVar = "I'm global";
-
-function myFunctionScope() {
-    // Local scope variable
-    let localVar = "I'm local"; 
-    console.log(localVar); // Output: I'm local
-    console.log(globalVar); // Output: I'm global
-}
-
-// Trying to access localVar outside the function will result in an error
-// console.log(localVar); // This would cause an error
-myFunctionScope();
-
-console.log("3. Global Scope:");
-
-let FruitName = "Apple"; // Global scope
-
-function myGlobalFunction() {
-    // This function can also use  FruitName
-    console.log(FruitName)
-}
-
-myGlobalFunction()
+// console.table()
+let uname = "abhi";
+let age = 20;
+console.table({ uname, age });
